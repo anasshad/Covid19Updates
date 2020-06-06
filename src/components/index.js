a07b7d6d96b42678b0eb06dec7ccbec5c7418b49
Anas Shad <anasshad@hotmail.com>
2020-06-06 09:48:20 -0700

    Added No data component

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
-export {default as Cards} from './Cards/Cards';
-export {default as Chart} from './Chart/Chart';
-export {default as Map} from './Map/Map';
-export {default as CountryPicker} from './CountryPicker/CountryPicker';
+export { default as Cards } from "./Cards/Cards";
+export { default as Chart } from "./Chart/Chart";
+export { default as Map } from "./Map/Map";
+export { default as NoData } from "./NoData/NoData";
+export { default as CountryPicker } from "./CountryPicker/CountryPicker";
