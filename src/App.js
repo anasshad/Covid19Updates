64441d3c6985788700858a40eba287b20fed4ec5
Anas Shad <anasshad@hotmail.com>
2020-06-06 06:14:36 -0700

    Added Grid for md

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import ReactTooltip from "react-tooltip";
 import styles from "./App.module.css";
+import { Grid, Typography } from "@material-ui/core";
 import { fetchData } from "./api";
 import { Cards, Chart, CountryPicker, Map } from "./components";
 import Logo from "./assets/COVID19LOGO.png";
@@ -20,10 +21,25 @@ export default function App() {
 
   return (
     <div className={styles.container}>
-      <img src={Logo} className={styles.image} alt="logo" />
-      <CountryPicker value={country} onChange={setCountry} />
-      <Cards data={data} />
-      <Map setTooltipContent={setContent} setCountry={setCountry} />
+      <Grid container justify="center" alignItems="center">
+        <Grid item xs={12} md={6}>
+          <img src={Logo} className={styles.image} alt="logo" />
+        </Grid>
+        <Grid item xs={12} md={5}>
+          <Typography variant="subtitle1" color="textSecondary" gutterBottom>
+            Select any country from options or map below to view updates
+          </Typography>
+          <CountryPicker value={country} onChange={setCountry} />
+        </Grid>
+      </Grid>
+      <Grid container alignItems="flex-start">
+        <Grid item xs={12} md={6}>
+          <Cards data={data} />
+        </Grid>
+        <Grid item xs={12} md={5}>
+          <Map setTooltipContent={setContent} setCountry={setCountry} />
+        </Grid>
+      </Grid>
       <Chart data={data} country={country} />
       <ReactTooltip>{content}</ReactTooltip>
     </div>
